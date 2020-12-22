af9a9231122738594233d57ada135e088b26d2e7
UdithaIshan <uditha.ishan34@gmail.com>
2020-12-22 08:55:47 +0530

    title changed

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Sindu Music Player");
+			primaryStage.setTitle("Sindu - Music Player");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
